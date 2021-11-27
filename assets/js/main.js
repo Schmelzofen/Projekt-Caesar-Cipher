792ad927522c4c92d32fc835883c7f622c087041
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-27 17:29:31 +0100

    Entwurf ohne Style

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let myArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+let myArray2 = new Proxy(myArray, {
+  get(target,prop){
+    if(!isNaN(prop)){
+      prop = parseInt(prop, 10)
+      if (prop < 0) {
+        prop += target.length
+      }
+    }
+    return target[prop]
+  }
+})
+console.log(myArray2[0])
+verschluesseln = () => {
+  let x = ""
+  let input = document.getElementById("inputText")
+  let input1 = input.value.toUpperCase().split("")
+  let ergebnis = document.getElementById("ergebnis")
+  let decode = document.getElementById("decodeNumber").value
+  for(i=0;i<input1.length;i++){
+    for(j=0;j<myArray.length;j++){
+      if(input1[i] == myArray[j]){
+        x += myArray2[j+Number(decode)]
+        ergebnis.innerHTML = x
+      }
+    }
+  }
+}
+
